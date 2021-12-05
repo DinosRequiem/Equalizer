ad1e07565d0460435829dbcccc5ba17ca07cd241
Joshua Sonnenfroh <dinosrequiem@gmail.com>
2021-12-05 02:23:43 -0500

    update adding equlaizer
    visualizer not requesting access to mic, or showing in the field

--- script.js
+++ script.js
@@ -1,65 +1,91 @@
+import {hslToRgb} from "./util"
+const WIDTH = 1500;
+const HEIGHT = 1500;
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d")
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+let analyzer;
+let bufferLength;
+const barWidth = (WIDTH / bufferLength) * 2.5;
+const x = 0;
 
-window.onload = function() {
-  
-    var file = document.getElementById("thefile");
-    var audio = document.getElementById("audio");
+
+//get sound
+async function getAudio() {
+ const stream = await navigator.mediaDevices.getUserMedia({audio: true}).catch(handleError);
+
+ const audioCtx = new AudioContext;
+ analyzer = audioCtx.createAnalyser();
+ const source = audioCtx.createMediaStreamSource(stream);
+
+ //Data Cap
+ analyzer.fftSize = 2 ** 10;
+ //Retrieve the data
+ bufferLength = analyzer.frequencyBinCount;
+ const timeData = new Uint8Array(bufferLength);
+ console.log(timeData);
+ const frequencyData = new Uint8Array(bufferLength);
+ console.log(frequencyData);
+ drawTimeData(timeData);
+ drawFrequency(frequencyData);
+}
+
+getAudio();
+
+function handleError(err) {
+    console.log("You must give access to your mic in order to proceed.")
+}
+
+function drawTimeData(timeData) {
+    analyzer.getByteTimeDomainData(timeData);
+    console.log(timeData);
     
-    file.onchange = function() {
-      var files = this.files;
-      audio.src = URL.createObjectURL(files[0]);
-      audio.load();
-      audio.play();
-      var context = new AudioContext();
-      var src = context.createMediaElementSource(audio);
-      var analyser = context.createAnalyser();
-  
-      var canvas = document.getElementById("canvas");
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
-      var ctx = canvas.getContext("2d");
-  
-      src.connect(analyser);
-      analyser.connect(context.destination);
-  
-      analyser.fftSize = 256;
-  
-      var bufferLength = analyser.frequencyBinCount;
-      console.log(bufferLength);
-  
-      var dataArray = new Uint8Array(bufferLength);
-  
-      var WIDTH = canvas.width;
-      var HEIGHT = canvas.height;
-  
-      var barWidth = (WIDTH / bufferLength) * 2.5;
-      var barHeight;
-      var x = 0;
-  
-      function renderFrame() {
-        requestAnimationFrame(renderFrame);
-  
-        x = 0;
-  
-        analyser.getByteFrequencyData(dataArray);
-  
-        ctx.fillStyle = "#000";
-        ctx.fillRect(0, 0, WIDTH, HEIGHT);
-  
-        for (var i = 0; i < bufferLength; i++) {
-          barHeight = (3 * dataArray[i]);
-          
-          var r = barHeight + (5 * (i/bufferLength));
-          var g = 200 * (i/bufferLength);
-          var b = 1000 * (i/bufferLength);
-  
-          ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-          ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
-  
-          x += barWidth + 1;
-        }
-      }
-  
-      audio.play();
-      renderFrame();
-    };
-  };
+    //setup canvas drawing
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = "#ffc600";
+    ctx.beginPath();
+    const sliceWidth = WIDTH / bufferLength;
+    console.log(sliceWidth);
+    let x = 0;
+    timeData.forEach((data, i) => {
+     const v = data / 128;   
+     const y = (v * HEIGHT) / 2; 
+     //clear canvas
+     ctx.clearRect(0, 0, WIDTH, HEIGHT);
+     //draw lines
+     if (i === 0) {
+         ctx.moveTo(x, y);
+     }  else {
+         ctx.lineTo(x, y)
+     }
+     x += sliceWidth;
+    });
+
+    ctx.stroke();
+
+    //call itself as soon as possible
+    requestAnimationFrame(() => drawTimeData(timeData));
+
+}
+
+function drawFrequency(frequencyData) {
+    //get data into the array
+    analyzer.getByteFrequencyData(frequencyData);
+    console.log(frequencyData);
+    requestAnimationFrame(() => drawFrequency(frequencyData));
+}
+
+frequencyData.forEach((amount) => {
+    //0-255
+    const percent = amount / 255;
+    const [h, s, l] = [360 / (percent * 360) - 0.5,, 0.8, 0.75];
+    const barHeight = HEIGHT * percent * 2;
+    //convert collor to HSL
+    const [r, g, b] = hslToRgb(h, s, l);
+    ctx.fillStyle = "rgb(${r},${g},${b})";
+    ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+    x += barWidth + 1;
+});
+
+
